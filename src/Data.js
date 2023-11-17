80e34ce552b95b83e4d89b11f656bbd9515720f9
Vinh Phan <vinh.phantrung1910@gmail.com>
2023-11-17 19:47:21 +0700

    first commit

---
+++ src/Data.js
@@ -0,0 +1,30 @@
+let shopItemsData = [
+  {
+    id: "LCRC2L",
+    name: "Nước rửa chén organic Layer Clean túi 2L",
+    price: 135000,
+    desc: "100% tự nhiên.",
+    img: "images/LC-RC-2L.png",
+  },
+  {
+    id: "LCRC5L",
+    name: "Nước rửa chén organic Layer Clean can 5L",
+    price: 231075,
+    desc: "100% tự nhiên.",
+    img: "images/LC-RC-5L.png",
+  },
+  {
+    id: "LCRC800ml",
+    name: "Nước rửa chén organic Layer Clean chai 800ml",
+    price: 67500,
+    desc: "100% tự nhiên.",
+    img: "images/LC-RC-800ml.png",
+  },
+  {
+    id: "LCNG5L",
+    name: "Nước giặt sinh học Layer Clean 5L Hương gió biển",
+    price: 296250,
+    desc: "100% tự nhiên.",
+    img: "images/LC-NG-5L.png",
+  },
+];
